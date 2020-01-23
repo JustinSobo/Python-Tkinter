65c02c7190de8e80aa62b331858ad53dffd1c897
Justin Stevens <37387224+JustinSobo@users.noreply.github.com>
2020-01-23 12:27:01 -0500

    Update b. Tkinter Intro.py

--- b. Tkinter Intro.py
+++ b. Tkinter Intro.py
@@ -1,1 +1,12 @@
+From tkinter import *
 
+#Initial Window that program runs in.
+root = Tk()
+
+#Creating a Label Widget.
+myLabel = Label(root, text="Hello World!")
+#Shoving Label Widget onto the screen.
+myLabel.pack()
+
+#End of initial Window that program runs in.
+root.mainloop()
