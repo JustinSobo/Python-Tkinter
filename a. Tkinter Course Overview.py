3344bb657854f88e3d8f268e71274acc82d4955e
Justin Stevens <37387224+JustinSobo@users.noreply.github.com>
2020-01-23 09:16:11 -0500

    Create a. Tkinter Course Overview.py

---
+++ a. Tkinter Course Overview.py
@@ -0,0 +1,24 @@
+Description: The list below describes the order in which the course will be conducted in.
+
+# 1. What is Tkinter?
+# 2. Positioning with Tkinter's Grid System
+# 3. Creating Buttons
+# 4. Creating Input Fields
+# 5. Building a Simple Calculator App
+# 6. Using Icons, Images, and Exit Buttons
+# 7. Building an Image Viewer App
+# 8. Adding a Status Bar
+# 9. Adding Frames
+# 10. Using Radio Buttons
+# 11. Adding Message Boxes
+# 12. Creating New Windows
+# 13. Opening Files Dialog Boxes
+# 14. Using Sliders
+# 15. Using Checkboxes
+# 16. Using Dropdown Menus
+# 17. Using Databases
+# 18. Building a GUI for a Database App
+# 19. Deleting a Record From a Database
+# 20. Updating a Record with SQLite
+# 21. Building a Weather App
+# 22. Using Matplotlib Charts
