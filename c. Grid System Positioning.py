b6a8c162c1fe41bb6cb21d2d53eebb96c91d5017
Justin Stevens <37387224+JustinSobo@users.noreply.github.com>
2020-01-23 21:56:28 -0500

    Update c. Grid System Positioning.py

--- c. Grid System Positioning.py
+++ c. Grid System Positioning.py
@@ -1,1 +1,15 @@
+from tkinter import *
 
+root = Tk()
+
+#Creating a Label Widget.
+myLabel1 = Label(root, text="Hello World!")
+myLabel2 = Label(root, text="My name is Justin.")
+myLabel3 = Label(root, text="                    ")
+
+#Shoving Label Widget onto the screen.
+myLabel1.grid(row=0, column=0)
+myLabel2.grid(row=1, column=5)
+myLabel3.grid(row=1, column=1)
+
+root.mainloop()
